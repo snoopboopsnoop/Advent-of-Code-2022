289ad5a1446da32f2d8f711b6192a5e88d2f0127
snoopboopsnoop <chengwaltj@gmail.com>
2022-12-03 23:31:37 -0800

    Completed Day 4

---
+++ Day 04/Day4Part1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <utility>
+
+using namespace std;
+
+// honestly embarrassing if you can't solve this one
+
+pair <int, int> getElf(ifstream& in);
+
+int main() {
+    // input stream
+    ifstream in("input.txt");
+
+    // broke
+    if(!in) cerr << "oops tehre was a fucky wucky" << endl;
+
+    int result = 0;
+
+    while(in) {
+        // initialize 2 elves
+        pair<int, int> elf1 = getElf(in);
+        pair<int, int> elf2 = getElf(in);
+
+        // check if bigger first has a smaller second
+        if((elf1.first >= elf2.first && elf1.second <= elf2.second)
+            || (elf1.first <= elf2.first && elf1.second >= elf2.second)) result++;
+    }
+    
+    cout << result << " overlapping pairs" << endl;
+
+    return 0;
+}
+
+pair <int, int> getElf(ifstream& in) {
+    int u, v;
+    in >> u;
+    in.get();
+    in >> v;
+    in.get();
+    return pair<int, int> (u, v);
+}

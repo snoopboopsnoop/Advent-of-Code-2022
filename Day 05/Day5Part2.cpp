0e0dadd6aa0e03fce541e7562c4c6a619a4a9dce
snoopboopsnoop <chengwaltj@gmail.com>
2022-12-04 22:18:56 -0800

    Completed Day 5

---
+++ Day 05/Day5Part2.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+// difficulty leap from day 4 to this one is bigger than your mother
+
+int main() {
+    // input stream
+    ifstream in("input.txt");
+
+    // broke
+    if(!in) cerr << "oops tehre was a fucky wucky" << endl;
+
+    string result;
+    string line;
+
+    getline(in, line);
+    int stacks = (line.size() + 1) / 4;
+    
+    vector<vector<char>> crates(stacks, vector<char>());
+    
+    // PART 1: parse top crate diagram into 2d vector
+    while(in) {
+        //check for end of diagram
+        if(line[1] == '1') break;
+
+        // remove spaces between each crate on the line
+        for(size_t i = 3; i < line.size(); i+=3) {
+            line.erase(line.begin() + i);
+        }
+
+        // add the character from the crate to its respective column vector
+        for(size_t i = 1; i < line.size(); i+=3) {
+            char c = line[i];
+            int stack = (i-1) / 3;
+
+            if(c == ' ') continue;
+            crates[stack].insert(crates[stack].begin(), c);
+        }
+        getline(in, line);
+    }
+
+    // throw away blank line
+    getline(in, line);
+
+    // PART 2: move the shit 
+    string trash;
+    int quantity = 0;
+    int start = 0;
+    int dest = 0;
+
+    while(getline(in, line)) {
+        // check for eof
+        if(line == "") break;
+
+        // read directions into variables
+        istringstream sin(line);
+        sin >> trash >> quantity >> trash >> start >> trash >> dest;
+
+        // index 1 crates -> index 0 vectors
+        start--;
+        dest--;
+
+        auto itr = crates[start].end()-quantity;
+        
+        while(itr != crates[start].end()) {
+            crates[dest].push_back(*itr);
+            crates[start].erase(itr);
+        }
+    }
+
+    for(auto i : crates) {
+        result += i.back();
+    }
+
+    cout << "result: " << result << endl;
+
+    return 0;
+}

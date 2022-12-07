14e6fad097b77d5ed2ae79ed68bc497b850dc395
snoopboopsnoop <chengwaltj@gmail.com>
2022-12-06 23:55:25 -0800

    Updated Day 7

--- Day 07/Day7Part1.cpp
+++ Day 07/Day7Part1.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <vector>
 #include <sstream>
 #include <cctype>
@@ -9,9 +8,11 @@ using namespace std;
 
 // difficulty leap from day 6 up to this one is bigger than your mother
 
+const int kCeiling = 100000;
+
 enum fileType {
-        dir, file
-    };
+    dir, file
+};
 
 struct Node {
     fileType type;
@@ -19,17 +20,28 @@ struct Node {
     int value;
     vector<Node*>* next;
     Node* previous;
+
+    // i have no clue if this works, if not I'm definitely leaking memory but ¯\_(ツ)_/¯
+    ~Node() {
+        for(auto& i : *next) {
+            if(i->type == 1) delete i;
+            delete[] i;
+        }
+    }
 };
 
-vector<Node*>& addNodes(ifstream& in, Node* curr) {
+// attaches vector of Nodes to curr according to file input
+vector<Node*>* addNodes(ifstream& in, Node* curr) {
     vector<Node*>* subtree = new vector<Node*>;
     string line;
+
     while(getline(in, line)) {
-        if(line == "") break;
+        // file display in form "[size] [name]"
+        // dir display in form "dir [name]"
         string first, second;
         istringstream sin(line);
-
         sin >> first >> second;
+
         if(isdigit(first[0])) {
             //cout << "found file " << second << endl;
             subtree->push_back(new Node{fileType::file, second, stoi(first), nullptr, curr});
@@ -38,47 +50,58 @@ vector<Node*>& addNodes(ifstream& in, Node* curr) {
             //cout << "found dir " << second << endl;
             subtree->push_back(new Node{fileType::dir, second, -1, new vector<Node*>, curr});
         }
-        if(in.peek() == '$' || '\0') {
+        // check for new command
+        if(in.peek() == '$') {
             break;
         }
     }
-    return *subtree;
+    return subtree;
 }
 
+// DEBUG function, prints the tree starting from given root
 ostream& operator<<(ostream& out, Node* root) {
     Node* curr = root;
-    if(curr->next == nullptr) return out;
+    if(curr->type == 1) return out;
 
     out << "Node " << curr->name << " points to ";
     for(const auto& i : *(curr->next)) {
+        // extra comma at the end of everything but who gives a shit
         out << i->name << ", ";
     }
     out << endl;
     for(const auto& i : *(curr->next)) {
         out << i;
     }
+
     return out;
 }
 
-int size_of(Node* root) {
+// returns size of a directory node (the sum of all its files/subfiles)
+// recursion !
+int size_of(Node* node) {
     int result = 0;
-    Node* curr = root;
-    if(curr->next == nullptr) return curr->value;
+    Node* curr = node;
+    if(curr->type == 1) return curr->value;
 
     for(const auto& i : *(curr->next)) {
         result += size_of(i);
     }
+
     return result;
 }
 
+// returns the sum of all directory nodes with total size under kCeiling (100,000)
+// (what kind of dumbass would accidentally do 10,000 not me that's for sure)
+// moar recursion !
 int solve(Node* root) {
     int result = 0;
     int temp = 0;
 
-    if(root->next == nullptr) return 0; 
+    // skip over file nodes
+    if(root->type == 1) return 0; 
 
     temp = size_of(root);
-    if(temp <= 100000 && temp > 0) {
+    if(temp <= kCeiling && temp > 0) {
         result += temp;
     }
     for(auto& i : *(root->next)) {
@@ -98,47 +121,46 @@ int main() {
     int result = 0;
     string line;
 
+    // establish root directory, set to looking at it
     Node* root = new Node{fileType::dir, "/", -1, new vector<Node*>, nullptr};
     Node* curr = root;
 
+    // throw away root line (assuming there is always a root directory)
     getline(in, line);
 
     while(getline(in, line)) {
-        if(line == "") break;
-
         istringstream sin(line);
+        // by structure of filesystem, token will always be $
         string token;
-        sin >> token;
-        if(token == "$") {
-            string command;
-            sin >> command;
-            if(command == "ls") {
-                //cout << "looking in dir " << curr->name << endl;
-                curr->next = &addNodes(in, curr);
+        string command;
+        sin >> token >> command;
+        if(command == "ls") {
+            //cout << "looking in dir " << curr->name << endl;
+            curr->next = addNodes(in, curr);
+        }
+        else if(command == "cd") {
+            string dir;
+            sin >> dir;
+            if(dir == "..") {
+                //cout << "going back" << endl;
+                curr = curr->previous;
             }
-            else if(command == "cd") {
-                string dir;
-                sin >> dir;
-                if(dir == "..") {
-                    //cout << "going back" << endl;
-                    curr = curr->previous;
-                }
-                else {
-                    for(const auto& i : *(curr->next)) {
-                        if(i->name == dir) {
-                            curr = i;
-                            //cout << "entering " << curr->name << endl;
-                        }
+            else {
+                for(const auto& i : *(curr->next)) {
+                    if(i->name == dir) {
+                        curr = i;
+                        //cout << "entering " << curr->name << endl;
                     }
                 }
             }
         }
     }
-    
+    //cout << root << endl;
+
     result = solve(root);
+    cout << "Sum of directories with a total size of under " << kCeiling << ": " << result << endl;
 
-    cout << "result: " << result << endl;
-
+    delete root;
     return 0;
 }
 
--- Day 07/Day7Part2.cpp
+++ Day 07/Day7Part2.cpp
@@ -9,12 +9,12 @@ using namespace std;
 
 // difficulty leap from day 6 up to this one is bigger than your mother
 
-const int totalSpace = 70000000;
-const int spaceReq = 30000000;
+const int kTotalSpace = 70000000;
+const int kMinSpace = 30000000;
 
 enum fileType {
-        dir, file
-    };
+    dir, file
+};
 
 struct Node {
     fileType type;
@@ -22,82 +22,95 @@ struct Node {
     int value;
     vector<Node*>* next;
     Node* previous;
+
+    // i have no clue if this works, if not I'm definitely leaking memory but ¯\_(ツ)_/¯
+    ~Node() {
+        for(auto& i : *next) {
+            if(i->type == 1) delete i;
+            delete[] i;
+        }
+    }
 };
 
-vector<Node*>& addNodes(ifstream& in, Node* curr) {
+// attaches vector of Nodes to curr through file input
+vector<Node*>* addNodes(ifstream& in, Node* curr) {
     vector<Node*>* subtree = new vector<Node*>;
     string line;
+
     while(getline(in, line)) {
-        if(line == "") break;
+        // file display in form "[size] [name]"
+        // dir display in form "dir [name]"
         string first, second;
         istringstream sin(line);
-
         sin >> first >> second;
+
         if(isdigit(first[0])) {
-            cout << "found file " << second << endl;
+            //cout << "found file " << second << endl;
             subtree->push_back(new Node{fileType::file, second, stoi(first), nullptr, curr});
         }
         else {
-            cout << "found dir " << second << endl;
+            //cout << "found dir " << second << endl;
             subtree->push_back(new Node{fileType::dir, second, -1, new vector<Node*>, curr});
         }
-        if(in.peek() == '$' || '\0') {
+        // check for new command
+        if(in.peek() == '$') {
             break;
         }
     }
-    return *subtree;
+    return subtree;
 }
 
+// DEBUG function, prints the tree starting from given root
 ostream& operator<<(ostream& out, Node* root) {
     Node* curr = root;
-    if(curr->next == nullptr) return out;
+    if(curr->type == 1) return out;
 
     out << "Node " << curr->name << " points to ";
     for(const auto& i : *(curr->next)) {
+        // extra comma at the end of everything but who gives a shit
         out << i->name << ", ";
     }
     out << endl;
     for(const auto& i : *(curr->next)) {
         out << i;
     }
+
     return out;
 }
 
-int size_of(Node* root) {
+// returns size of a directory node (the sum of all its files/subfiles)
+// recursion !
+int size_of(Node* node) {
     int result = 0;
-    Node* curr = root;
-    if(curr->next == nullptr) return curr->value;
+    Node* curr = node;
+    if(curr->type == 1) return curr->value;
 
     for(const auto& i : *(curr->next)) {
         result += size_of(i);
     }
+
     return result;
 }
 
-int size_of_helper(Node* root) {
-    int result = size_of(root);
-    //cout << "checking size of " << root->name << ": ";
-    //cout << result << endl;
-    return result;
-}
-
+// returns the smallest of all directory nodes with size of at least spaceNeeded
+// moar recursion !
 int solve(Node* root, int spaceNeeded, int prev) {
-    //cout << "checking node " << root->name << " to get at least " << spaceNeeded << ", best option is " << prev << endl;
     int result = prev;
     int temp = 0;
 
+    // skip file nodes
     if(root->next == nullptr) return 0; 
 
-    temp = size_of_helper(root);
+    // check root
+    temp = size_of(root);
     if(temp >= spaceNeeded && temp < prev) {
         result = temp;
-        //cout << "can delete node " << root->name << " to save " << result << " storage" << endl;
     }
+    // check everything under root
     for(auto& i : *(root->next)) {
         temp = solve(i, spaceNeeded, result);
         if(temp >= spaceNeeded && temp < prev) {
             result = temp;
-            //cout << "can delete node " << root->name << " to save " << result << " storage" << endl;
         }
     }
 
@@ -114,54 +127,50 @@ int main() {
     int result = 0;
     string line;
 
+    // establish root directory, set to looking at it
     Node* root = new Node{fileType::dir, "/", -1, new vector<Node*>, nullptr};
     Node* curr = root;
 
+    // throw away root line (assuming there is always a root directory)
     getline(in, line);
 
     while(getline(in, line)) {
-        if(line == "") break;
-
         istringstream sin(line);
+        // by structure of filesystem, token will always be $
         string token;
-        sin >> token;
-        if(token == "$") {
-            string command;
-            sin >> command;
-            if(command == "ls") {
-                cout << "looking in dir " << curr->name << endl;
-                curr->next = &addNodes(in, curr);
+        string command;
+        sin >> token >> command;
+        if(command == "ls") {
+            //cout << "looking in dir " << curr->name << endl;
+            curr->next = addNodes(in, curr);
+        }
+        else if(command == "cd") {
+            string dir;
+            sin >> dir;
+            if(dir == "..") {
+                //cout << "going back" << endl;
+                curr = curr->previous;
             }
-            else if(command == "cd") {
-                string dir;
-                sin >> dir;
-                if(dir == "..") {
-                    cout << "going back" << endl;
-                    curr = curr->previous;
-                }
-                else {
-                    for(const auto& i : *(curr->next)) {
-                        if(i->name == dir) {
-                            curr = i;
-                            cout << "entering " << curr->name << endl;
-                        }
+            else {
+                for(const auto& i : *(curr->next)) {
+                    if(i->name == dir) {
+                        curr = i;
+                        //cout << "entering " << curr->name << endl;
                     }
                 }
             }
         }
     }
+    //cout << root << endl;
 
-    //cout << size_of_helper(root) << endl;
-    int usedSpace = size_of_helper(root);
-    int spaceNeeded = spaceReq - (totalSpace - usedSpace);
+    // calculate the minimum size of directory to delete (spaceNeeded)
+    int usedSpace = size_of(root);
+    int spaceNeeded = kMinSpace - (kTotalSpace - usedSpace);
+    //cout << "need to delete " << spaceNeeded << " amount of storage" << endl;
 
+    result = solve(root, spaceNeeded, usedSpace);
+    cout << "Directory to delete increases space by: " << result << endl;
 
-    cout << "need to delete " << spaceNeeded << " amount of storage" << endl;
-
-    result = solve(root, spaceNeeded, usedSpace+1);
-
-    cout << "result: " << result << endl;
-
+    delete root;
     return 0;
 }
-

4e558910812bf860b99ab86a0346faf2a3dae7c4
snoopboopsnoop <chengwaltj@gmail.com>
2022-12-08 21:22:15 -0800

    Completed Day 08 (Needs HEAVY cleaning)

--- Day 08/Day8Part2.cpp
+++ Day 08/Day8Part2.cpp
@@ -11,13 +11,13 @@ int getScenicScore(const size_t row, const size_t column, const vector<string>& 
     int result = 1;
     
     int val = map[row][column];
-    cout << "checking tree at row " << row << ", column " << column << " with value " << val << endl;
+    //cout << "checking tree at row " << row << ", column " << column << " with value " << val-'0' << endl;
 
     size_t rowEnd = map.size();
     size_t columnEnd = map[0].size();
     // check top
     int temp = 0;
-    for(size_t i = 0; i < row; ++i) {
+    for(int i = row-1; i > -1; --i) {
         //cout << "looking at tree at row " << i << ", column " << column << " with value " << map[i][column] << endl;
         if(map[i][column] >= val) {
             temp++;
@@ -25,12 +25,12 @@ int getScenicScore(const size_t row, const size_t column, const vector<string>& 
         }
         temp++;
     }
-    cout << "from top, score: " << temp << endl;
+    //cout << "from top, score: " << temp << endl;
     if(temp > 0) result *= temp;
     
     // check bottom
     temp = 0;
-    for(size_t i = rowEnd-1; i > row; --i) {
+    for(int i = row+1; i < rowEnd; ++i) {
         //cout << "looking at tree at row " << i << ", column " << column << " with value " << map[i][column] << endl;
         if(map[i][column] >= val) {
             temp++;
@@ -38,11 +38,11 @@ int getScenicScore(const size_t row, const size_t column, const vector<string>& 
         }
         temp++;
     }
-    cout << "from bottom, score: " << temp << endl;
+    //cout << "from bottom, score: " << temp << endl;
     if(temp > 0) result *= temp;
     // check left
     temp = 0;
-    for(size_t i = 0; i <= column; ++i) {
+    for(int i = column-1; i > -1; --i) {
         //cout << "looking at tree at row " << row << ", column " << i << " with value " << map[row][i] << endl;
         if(map[row][i] >= val) {
             temp++;
@@ -50,11 +50,11 @@ int getScenicScore(const size_t row, const size_t column, const vector<string>& 
         }
         temp++;
     }
-    cout << "from left, score: " << temp << endl;
+    //cout << "from left, score: " << temp << endl;
     if(temp > 0) result *= temp;
     // check right
     temp = 0;
-    for(size_t i = columnEnd-1; i >= column; --i) {
+    for(int i = column+1; i < columnEnd; ++i) {
         //cout << "looking at tree at row " << row << ", column " << i << " with value " << map[row][i] << endl;
         if(map[row][i] >= val) {
             temp++;
@@ -62,10 +62,10 @@ int getScenicScore(const size_t row, const size_t column, const vector<string>& 
         }
         temp++;
     }
-    cout << "from right, score: " << temp << endl;
+    //cout << "from right, score: " << temp << endl;
     if(temp > 0) result *= temp;
 
-    cout << "score: " << to_string(result) << endl;
+    //cout << "score: " << to_string(result) << endl;
     return result;
 }
 
@@ -91,7 +91,10 @@ int main() {
     for(size_t row = 1; row < map.size()-1; ++row) {
         for(size_t column = 1; column < map[0].size()-1; ++column) {
             int temp = getScenicScore(row, column, map);
-            if(temp > result) result = temp;
+            if(temp > result) {
+                cout << "new best at row " << row << ", column " << column << " with score " << temp << endl;
+                result = temp;
+            }
         }
     }
 

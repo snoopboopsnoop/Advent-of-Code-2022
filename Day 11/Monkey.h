a278c6672e53473717b6dc55cda0f5939e4ec69f
snoopboopsnoop <chengwaltj@gmail.com>
2022-12-11 00:10:19 -0800

    Working on Day 11 but bullet train more important

--- Day 11/Monkey.h
+++ Day 11/Monkey.h
@@ -2,6 +2,39 @@
 #define MONKEY_H
 
 #include <string>
+#include <vector>
+
+class Monkey {
+public:
+    Monkey(std::vector<int>& items, std::vector<std::string>& operationVals, int testVal, int testTrue, int testFalse);
+    // run inspection on each item monkey is holding, return monkey to throw it to
+    int inspect();
+    int getInspectionCount();
+    // add item to items vector
+    void addItem(int item);
+    static std::vector<Monkey> monkeys();
+
+private:
+    // divide item by 3 and floor
+    void bored(int& item);
+    // run operation
+    void operation(int& item);
+    // run test case, return result
+    bool test(int& item);
+
+    std::vector<int> m_items;
+    // operation, val2
+    std::vector<std::string> m_operationVals;
+    // number to be divisble by
+    int m_testVal;
+    // keep track of number of items inspected
+    int m_inspectionCount;
+    // monkey # to throw to
+    int m_testTrue;
+    int m_testFalse;
+
+    static std::vector<Monkey> m_monkeys;
+};
 
 
 #endif
